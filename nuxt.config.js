9e5fb224fcff855e570cf20aff9d197eae3124db
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-05-29 19:17:58 +0900

    :wrench: change site name setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '診断チャートメーカー',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
