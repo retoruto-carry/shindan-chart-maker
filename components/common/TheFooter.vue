fb373fdd2d0cc5c768d8cacbff7a0dbdb059f6b5
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-05-30 05:10:10 +0900

    :art: フッターを追加

---
+++ components/common/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full bg-red-700 py-8 text-white text-center">
+    created by
+    <a
+      href="https://twitter.com/retoruto_carry"
+      target="_blank"
+      class="underline"
+    >
+      @retoruto_carry
+    </a>
+  </div>
+</template>
