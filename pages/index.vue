a266352ff69d5b1e830ee23526313c33c3b38489
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-05-29 18:49:00 +0900

    :+1: 診断一覧

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
-    <h1>トップページ</h1>
+    <h1 class="text-2xl font-bold text-red-600">診断チャートメーカー</h1>
+    <h2 class="text-xl font-bold mt-2">診断一覧</h2>
+    <PostList :posts="posts" class="mt-2" />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PostList from '~/components/partials/post/PostList.vue'
+import { Post } from '~/types/struct'
+import { toPost } from '~/utils/transformer/toObject'
+
+type LocalData = {
+  hasNext: boolean
+  isProcessing: boolean
+  posts: Post[]
+}
+
+export default Vue.extend({
+  components: {
+    PostList
+  },
+  async asyncData({ app }) {
+    const postDocuments = await app.$firestore
+      .collection('posts')
+      .orderBy('createdAt', 'desc')
+      .get()
+    const posts = postDocuments.docs.map(
+      (postDocument): Post => {
+        return toPost(postDocument)
+      }
+    )
+    return {
+      posts
+    }
+  },
+  data(): LocalData {
+    return {
+      hasNext: true,
+      isProcessing: false,
+      posts: []
+    }
+  }
+})
+</script>
