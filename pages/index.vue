1a4f55d97f3930467fa6de0cfd29611f9a030199
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-05-30 02:32:24 +0900

    :art: 診断を作るボタンを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="text-center">
     <h1 class="text-2xl font-bold text-red-600">診断チャートメーカー</h1>
-    <h2 class="text-xl font-bold mt-2">新着一覧</h2>
+    <div class="mt-12">
+      <p class="text-sm text-gray-500">超簡単♪</p>
+      <p class="text-red-600 mb-1">あなたも診断を作ってみよう</p>
+      <button
+        class="block mx-auto bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white py-1 px-4 border border-red-500 hover:border-transparent rounded"
+        @click="$router.push('/posts/create')"
+      >
+        <i class="mdi mdi-pencil mr-1" />
+        診断をつくる
+      </button>
+    </div>
+    <h2 class="text-xl font-bold mt-12">新着一覧</h2>
     <PostList :posts="posts" class="mt-2" />
     <button
       v-show="hasNext && posts.length >= LIMIT"
@@ -11,6 +22,17 @@
       <span v-if="!isProcessing">もっと見る</span>
       <div v-else class="spinner"></div>
     </button>
+    <div class="mt-12">
+      <p class="text-sm text-gray-500">超簡単♪</p>
+      <p class="text-red-600 mb-1">あなたも診断を作ってみよう</p>
+      <button
+        class="block mx-auto bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white py-1 px-4 border border-red-500 hover:border-transparent rounded"
+        @click="$router.push('/posts/create')"
+      >
+        <i class="mdi mdi-pencil mr-1" />
+        診断をつくる
+      </button>
+    </div>
   </div>
 </template>
 
