472e132df398ba0d6deea5e87dcb205e0c321bb1
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-05-29 19:51:21 +0900

    :+1: add infinity loading

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,14 @@
     <h1 class="text-2xl font-bold text-red-600">診断チャートメーカー</h1>
     <h2 class="text-xl font-bold mt-2">診断一覧</h2>
     <PostList :posts="posts" class="mt-2" />
+    <button
+      v-show="hasNext && posts.length >= LIMIT"
+      class="bg-red-500 hover:bg-red-700 text-white font-bold h-10 w-32 rounded mt-4"
+      @click="readMorePosts"
+    >
+      <span v-if="!isProcessing">もっと見る</span>
+      <div v-else class="spinner"></div>
+    </button>
   </div>
 </template>
 
@@ -11,6 +19,8 @@ import Vue from 'vue'
 import PostList from '~/components/partials/post/PostList.vue'
 import { Post } from '~/types/struct'
 import { toPost } from '~/utils/transformer/toObject'
+
+const LIMIT: number = 15
 
 type LocalData = {
   hasNext: boolean
@@ -26,6 +36,7 @@ export default Vue.extend({
     const postDocuments = await app.$firestore
       .collection('posts')
       .orderBy('createdAt', 'desc')
+      .limit(LIMIT)
       .get()
     const posts = postDocuments.docs.map(
       (postDocument): Post => {
@@ -41,6 +52,34 @@ export default Vue.extend({
       hasNext: true,
       isProcessing: false,
       posts: []
+    }
+  },
+  computed: {
+    LIMIT(): number {
+      return LIMIT
+    }
+  },
+  methods: {
+    async readMorePosts(): Promise<void> {
+      this.isProcessing = true
+      try {
+        const postDocuments = await this.$firestore
+          .collection('posts')
+          .orderBy('createdAt', 'desc')
+          .startAfter(this.posts[this.posts.length - 1].createdAt)
+          .limit(LIMIT)
+          .get()
+        const posts = postDocuments.docs.map(
+          (postDocument): Post => {
+            return toPost(postDocument)
+          }
+        )
+        this.posts = [...this.posts, ...posts]
+        this.hasNext = posts.length === LIMIT
+      } catch (e) {
+      } finally {
+        this.isProcessing = false
+      }
     }
   }
 })
