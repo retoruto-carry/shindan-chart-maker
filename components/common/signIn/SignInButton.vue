92fca041ad5a7f9e7b8cdb8f1be01ed54903b031
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-05-30 04:03:24 +0900

    :+1: ログイン時にローディングするようにした

--- components/common/signIn/SignInButton.vue
+++ components/common/signIn/SignInButton.vue
@@ -1,12 +1,17 @@
 <template>
   <sign-in>
-    <template v-slot:default="{ signIn }">
+    <template v-slot:default="{ signIn, isProcessing }">
       <button
         class="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded"
         @click="signIn()"
       >
-        <i class="mdi mdi-twitter" />
-        ログイン
+        <template v-if="!isProcessing">
+          <i class="mdi mdi-twitter" />
+          ログイン
+        </template>
+        <template v-else>
+          <div class="spinner h-6 w-16" />
+        </template>
       </button>
     </template>
   </sign-in>
--- components/common/signIn/SingIn.vue
+++ components/common/signIn/SingIn.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <client-only>
-      <slot v-if="!$auth.currentUser" :signIn="signIn" />
+      <slot
+        v-if="!$auth.currentUser"
+        :signIn="signIn"
+        :isProcessing="isProcessing"
+      />
     </client-only>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+
+type LocalData = {
+  isProcessing: Boolean
+}
+
+export default Vue.extend({
+  data(): LocalData {
+    return {
+      isProcessing: false
+    }
+  },
   methods: {
     async signIn() {
+      this.isProcessing = true
       const result = await this.$auth.signInWithPopup(
         new this.$firebase.auth.TwitterAuthProvider()
       )
@@ -19,12 +35,16 @@ export default {
           .doc(result.user.uid)
           .set({
             displayName: result.user.displayName,
-            photoUrl: result.user.photoURL.replace('normal', '400x400'),
-            username: result.additionalUserInfo.username
+            photoUrl: (result.user.photoURL as string).replace(
+              'normal',
+              '400x400'
+            ),
+            username: (result.additionalUserInfo as firebase.auth.AdditionalUserInfo)
+              .username
           })
         location.reload()
       }
     }
   }
-}
+})
 </script>
